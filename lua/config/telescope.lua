477ac09f8eaf3046f7f4834eb4311b7040a643ac
Yarnus <yarnus1993@hotmail.com>
2021-12-15 15:58:47 +0800

    updatd prefix.

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -13,7 +13,7 @@ telescope.setup({
             'rg', '--color=never', '--no-heading', '--with-filename',
             '--line-number', '--column', '--smart-case'
         },
-        prompt_prefix = '🔭 ',
+        prompt_prefix = '❯ ',
         selection_caret = ' ',
         entry_prefix = '  ',
         initial_mode = 'insert',
@@ -50,8 +50,8 @@ telescope.setup({
     },
     extensions = {
         fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = false, -- override the generic sorter
+            fuzzy = false, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true, -- override the file sorter
             case_mode = 'smart_case' -- or 'ignore_case' or 'respect_case'
             -- the default case_mode is 'smart_case'
