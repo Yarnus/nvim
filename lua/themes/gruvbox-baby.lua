1e8fb63796d41373a4965a2cd11a20f33cdc92b8
Yarnus <yarnus1993@hotmail.com>
2022-05-13 16:46:35 +0800

    updated visual highlight.

--- lua/themes/gruvbox-baby.lua
+++ lua/themes/gruvbox-baby.lua
@@ -1,18 +1,7 @@
--- Example config in Lua
+local vim = vim
 vim.g.gruvbox_baby_function_style = "italic"
 -- vim.g.gruvbox_baby_keyword_style = "italic"
-
--- Each highlight group must follow the structure:
--- ColorGroup = {fg = "foreground color", bg = "background_color", style = "some_style(:h attr-list)"}
--- See also :h highlight-guifg
--- Example:
--- vim.g.gruvbox_baby_highlights = { Normal = { fg = "#123123", bg = "NONE", style = "" } }
-
--- Enable telescope theme
+vim.g.gruvbox_baby_highlights = { Visual = { bg = '#fabd2f' } }
 vim.g.gruvbox_baby_telescope_theme = 1
-
--- Enable transparent mode
 vim.g.gruvbox_baby_transparent_mode = 0
-
--- Load the colorscheme
 vim.cmd [[colorscheme gruvbox-baby]]
