e46b3b8ec575282047f00d8bb30189208f37e89f
Yarnus <yarnus1993@hotmail.com>
2023-08-09 16:17:51 +0800

    updated buffline.

--- lua/config/nvim-bufferline.lua
+++ lua/config/nvim-bufferline.lua
@@ -1,21 +1,25 @@
-require('bufferline').setup {
+local bufferline = require('bufferline')
+bufferline.setup {
   highlights = {
-    -- fill = { bg = 'none' },
-    buffer_selected = { fg = '#08cd7d', bold = false, italic = false },
-    buffer_visible = { fg = '#928374' }
-    -- indicator_selected = { fg = '#08cd7d' },
-    -- separator = { bg = 'none' }
+    buffer_selected = { fg = '#08cd7d', bold = false, italic = true },
+    buffer_visible = { fg = '#928374' },
+    indicator_selected = { fg = '#08cd7d' }
+
   },
   options = {
-    hover = { enabled = false },
-    mode = "buffers",
+    mode = "buffers",                               -- set to "tabs" to only show tabpages instead
+    style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+    themable = true,                                -- allows highlight groups to be overriden i.e. sets highlights as default
     numbers = "none",
-    buffer_close_icon = '',
+    close_command = "bdelete! %d",                  -- can be a string | function, | false see "Mouse actions"
+    right_mouse_command = "bdelete! %d",            -- can be a string | function | false, see "Mouse actions"
+    left_mouse_command = "buffer %d",               -- can be a string | function, | false see "Mouse actions"
+    middle_mouse_command = nil,                     -- can be a string | function, | false see "Mouse actions"
     indicator = {
-      style = 'none',
-      -- icon = ' ',
-      icon = ''
+      style = 'icon',
+      icon = ' ', -- this should be omitted if indicator style is not 'icon'
     },
+    buffer_close_icon = '',
     modified_icon = '●',
     close_icon = '',
     -- left_trunc_marker = '',
@@ -26,21 +30,67 @@ require('bufferline').setup {
       end
     end,
     max_name_length = 18,
-    max_prefix_length = 15,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    truncate_names = true,  -- whether or not tab names should be truncated
     tab_size = 18,
-    color_icons = true,
-    show_buffer_close_icons = true,
-    show_tab_indicators = false,
-    separator_style = 'thin',
-    --mappings = true,
-    always_show_bufferline = true,
-    enforce_regular_tabs = true,
     diagnostics = false,
-    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --   return '("..count..")'
+    diagnostics_update_in_insert = false,
+    -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      return "(" .. count .. ")"
+    end,
+    -- NOTE: this will be called a lot so don't do any heavy processing here
+    -- custom_filter = function(buf_number, buf_numbers)
+    --     -- filter out filetypes you don't want to see
+    --     if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+    --         return true
+    --     end
+    --     -- filter out by buffer name
+    --     if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+    --         return true
+    --     end
+    --     -- filter out based on arbitrary rules
+    --     -- e.g. filter out vim wiki buffer from tabline in your work repo
+    --     if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+    --         return true
+    --     end
+    --     -- filter out by it's index number in list (don't show first buffer)
+    --     if buf_numbers[1] ~= buf_number then
+    --         return true
+    --     end
     -- end,
     offsets = {
       { filetype = 'NvimTree', text = ' Explorer', padding = 1 }
-    }
+    },
+    color_icons = true, -- whether or not to add the filetype icon highlights
+    -- get_element_icon = function(element)
+    --   -- element consists of {filetype: string, path: string, extension: string, directory: string}
+    --   -- This can be used to change how bufferline fetches the icon
+    --   -- for an element e.g. a buffer or a tab.
+    --   -- e.g.
+    --   local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+    --   return icon, hl
+    --   -- or
+    --   local custom_map = {my_thing_ft: {icon = "my_thing_icon", hl}}
+    --   return custom_map[element.filetype]
+    -- end,
+    show_buffer_icons = false, -- disable filetype icons for buffers
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = true,
+    show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+    persist_buffer_sort = true,   -- whether or not custom sorted buffers should persist
+    move_wraps_at_ends = false,   -- whether or not the move command "wraps" at the first or last position
+    -- can also be a table containing 2 custom separators
+    -- [focused and unfocused]. eg: { '|', '|' }
+    separator_style = "none",
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+    hover = {
+      enabled = false,
+      delay = 200,
+      reveal = { 'close' }
+    },
+    sort_by = 'insert_after_current'
   }
 }
