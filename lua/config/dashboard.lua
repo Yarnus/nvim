4ad81f97a903d5d7c2ea27288338a5ec4a3169f3
Yarnus <yarnus1993@hotmail.com>
2025-03-05 16:28:14 +0800

    use fzflua support recent projects.

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,48 +1,65 @@
 local db = require('dashboard')
 db.setup({
-  -- disable_move     -- default is false disable move keymap for hyper
-  -- shortcut_type    -- shortcut type 'letter' or 'number'
-  -- shuffle_letter   -- default is false, shortcut 'letter' will be randomize, set to false to have ordered letter
-  -- letter_list      -- default is a-z, excluding j and k
-  -- change_to_vcs_root -- default is false,for open file in hyper mru. it will change to the root of vcs
-  -- hide = {
-  --   statusline     -- hide statusline default is true
-  --   tabline        -- hide the tabline
-  --   winbar         -- hide winbar
-  -- },
-  -- preview = {
-  --   command   -- preview command
-  --   file_path -- preview file path
-  --   file_height -- preview file height
-  --   file_width -- preview file width
-  -- },
+  change_to_vcs_root = true, -- 启用切换到版本控制系统根目录
   theme = 'hyper',
   config = {
-    week_header = {
-      enable = true,
-    },
+    week_header = { enable = true },
     shortcut = {
       { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-      -- {
-      --   icon = ' ',
-      --   icon_hl = '@variable',
-      --   desc = 'Files',
-      --   group = 'Label',
-      --   action = 'FzfLua files',
-      --   key = 'f',
-      -- },
-      -- {
-      --   desc = ' Apps',
-      --   group = 'DiagnosticHint',
-      --   action = 'Telescope app',
-      --   key = 'a',
-      -- },
-      -- {
-      --   desc = ' dotfiles',
-      --   group = 'Number',
-      --   action = 'Telescope dotfiles',
-      --   key = 'd',
-      -- },
+      {
+        icon = ' ',
+        icon_hl = '@variable',
+        desc = 'Files',
+        group = 'Label',
+        action = 'FzfLua files',
+        key = 'f',
+      },
+      {
+        desc = ' Projects',
+        group = 'Number',
+        action = function()
+          require('fzf-lua').fzf_exec(
+            'find ~ -maxdepth 3 -type d -name ".git" | sed "s/\\.git$//"',
+            {
+              prompt = "Projects> ",
+              actions = {
+                ["default"] = function(selected)
+                  local dir = selected[1]
+                  vim.cmd('cd ' .. vim.fn.fnameescape(dir))
+                  require('fzf-lua').files({ cwd = dir })
+                end
+              },
+              winopts = {
+                height = 0.85,
+                width = 0.80,
+                row = 0.35,
+                col = 0.50,
+                border = "rounded",
+              }
+            }
+          )
+        end,
+        key = 'p',
+      },
+      {
+        desc = ' Recent Files',
+        group = 'String',
+        action = 'FzfLua oldfiles',
+        key = 'r',
+      },
+    },
+    project = {
+      enable = true,
+      limit = 8,
+      icon = ' ',
+      label = 'Recent Projects:',
+      action = 'FzfLua files cwd=',
+    },
+    mru = {
+      enable = false,
+      limit = 10,
+      icon = ' ',
+      label = 'Most Recent Files:',
     },
   },
 })
