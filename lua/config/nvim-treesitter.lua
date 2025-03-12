b51e814413b854209e71c52b67d8a35d8b7ace01
Yarnus <yarnus1993@hotmail.com>
2025-03-12 10:35:06 +0800

    feat: disable markdown highlight.

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,11 +1,14 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "c", "c_sharp", "elixir", "yaml", "dockerfile", "html", "vue", "javascript", "json", "python",
-    "json5", "typescript", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+    "json5", "typescript", "lua", "vim", "vimdoc" },
   ignore_install = {},
   auto_install = true,
   highlight = {
     enable = true,
     disable = function(lang, buf)
+      if lang == "markdown" then
+        return true
+      end
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
