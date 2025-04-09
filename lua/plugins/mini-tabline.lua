4916ac4da00c86f3522e8c994ff881a5be9c3c8e
Yarnus <yarnus1993@hotmail.com>
2025-04-09 16:09:59 +0800

    feat: updated tabline.

--- lua/plugins/mini-tabline.lua
+++ lua/plugins/mini-tabline.lua
@@ -1,15 +1,11 @@
 return {
 	"echasnovski/mini.tabline",
 	version = "*",
-	dependencies = { "echasnovski/mini.icons" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		-- Whether to show file icons (requires 'mini.icons')
-		show_icons = true,
-
-		-- Function which formats the tab label
-		-- By default surrounds with space and possibly prepends with icon
+		show_icons = false,
 		format = nil,
-
 		-- Where to show tabpage section in case of multiple vim tabpages.
 		-- One of 'left', 'right', 'none'.
 		tabpage_section = "left",
@@ -18,10 +14,12 @@ return {
 		require("mini.tabline").setup(opts)
 
 		-- 设置与kanagawa-paper配色方案匹配的高亮组
-		vim.api.nvim_set_hl(0, "MiniTablineCurrent", { fg = "#1F1F28", bg = "#DCD7BA", italic = true })
-		vim.api.nvim_set_hl(0, "MiniTablineVisible", { fg = "#DCD7BA", bg = "#363646" })
-		vim.api.nvim_set_hl(0, "MiniTablineHidden", { fg = "#C8C093", bg = "#2A2A37" })
-		vim.api.nvim_set_hl(0, "MiniTablineFill", { fg = "none", bg = "none" })
-		vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { fg = "#1F1F28", bg = "#DCD7BA", italic = true })
+		vim.api.nvim_set_hl(0, "MiniTablineFill", { fg = "", bg = "" })
+		vim.api.nvim_set_hl(0, "MiniTablineCurrent", { fg = "#7FB4CA", bg = "#eeeeee", italic = false, bold = true })
+		vim.api.nvim_set_hl(0, "MiniTablineVisible", { fg = "#8e908c", bg = "#eeeeee", italic = true })
+		vim.api.nvim_set_hl(0, "MiniTablineHidden", { fg = "#8e908c", bg = "#eeeeee", italic = true })
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { fg = "#DCA561", bg = "#eeeeee", bold = true })
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", { fg = "#DCA561", bg = "#eeeeee", bold = true })
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", { fg = "#DCA561", bg = "#eeeeee", bold = true })
 	end,
 }
