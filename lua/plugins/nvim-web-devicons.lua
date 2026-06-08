b0228627cb989bf5a9e7f4e4252a7811eb7e91d0
Yarnus <yarnus1993@hotmail.com>
2026-06-08 16:25:28 +0800

    Override YAML devicon

---
+++ lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-tree/nvim-web-devicons",
+	opts = {
+		override_by_extension = {
+			yml = {
+				icon = "",
+				color = "#d70000",
+				name = "Yaml",
+			},
+			yaml = {
+				icon = "",
+				color = "#d70000",
+				name = "Yaml",
+			},
+		},
+	},
+}
